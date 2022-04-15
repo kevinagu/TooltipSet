4e8970f9ffcb908acc28cbdecd8edf8a6695b88d
kagudelo90 <kevin.alonso@mercadolibre.com.co>
2022-04-15 09:05:57 -0500

    project created

---
+++ TooltipsSet/TooltipsSet.h
@@ -0,0 +1,18 @@
+//
+//  TooltipsSet.h
+//  TooltipsSet
+//
+//  Created by Kevin Alonso Agudelo Betancourt on 15/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TooltipsSet.
+FOUNDATION_EXPORT double TooltipsSetVersionNumber;
+
+//! Project version string for TooltipsSet.
+FOUNDATION_EXPORT const unsigned char TooltipsSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TooltipsSet/PublicHeader.h>
+
+
---
+++ TooltipsSetExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TooltipsSetExamples
+//
+//  Created by Kevin Alonso Agudelo Betancourt on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
